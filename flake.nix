218998ee44b9e01b3e2ca2aff5d9949cea18dc44
jeebuscrossaint <thediamond270@gmail.com>
2025-12-30 22:53:07 -0500

    Fix Flake

--- flake.nix
+++ flake.nix
@@ -1,26 +1,29 @@
 {
   description = "London Underground Dot Matrix Patched Nerd Fonts Flake";
-
   inputs.nixpkgs.url = "github:NixOS/nixpkgs/nixos-unstable";
-
   outputs = { self, nixpkgs }: {
-    packages.x86_64-linux.london-underground-dot-matrix-fonts = 
-      let
-        pkgs = import nixpkgs { system = "x86_64-linux"; };
-      in
-      pkgs.stdenv.mkDerivation {
-        pname = "london-underground-dot-matrix-fonts";
-        version = "1.0.0";
-        src = ./font;
-        installPhase = ''
-          install -Dm644 $src/*.{ttf,otf} -t $out/share/fonts/truetype
-        '';
-        meta = with pkgs.lib; {
-          description = "London Underground Dot Matrix Patched Nerd Fonts";
-          license = licenses.ofl;
+    packages.x86_64-linux = {
+      london-underground-dot-matrix-fonts = 
+        let
+          pkgs = import nixpkgs { system = "x86_64-linux"; };
+        in
+        pkgs.stdenv.mkDerivation {
+          pname = "london-underground-dot-matrix-fonts";
+          version = "1.0.0";
+          src = ./font;
+          installPhase = ''
+            install -Dm644 $src/*.{ttf,otf} -t $out/share/fonts/truetype
+          '';
+          meta = with pkgs.lib; {
+            description = "London Underground Dot Matrix Patched Nerd Fonts";
+            license = licenses.ofl;
+          };
         };
-      };
-
+      
+      # Add default as an alias
+      default = self.packages.x86_64-linux.london-underground-dot-matrix-fonts;
+    };
+    
     # For home-manager or system-wide use
     homeManagerModules.default = { config, ... }: {
       fonts.fonts = [ self.packages.x86_64-linux.london-underground-dot-matrix-fonts ];
